8f55f11d607e642e3add7827a7a932d17c834bbe
doubleDragon <aiai373824745@gmail.com>
2017-11-21 18:17:41 +0800

    Fix bithumb_bch.sh

--- script/bithumb_bch.sh
+++ script/bithumb_bch.sh
@@ -5,5 +5,5 @@ cd $base_dir/log
 rm -rf *.log
 
 cd $base_dir
-rm -rf screen*.log
+rm -rf screenlog.*
 ./venv/bin/python -m quant.cli -mBithumb_BCH_KRW,Bitfinex_BCH_BTC,Bithumb_BTC_KRW -o=T_Bithumb_BCH -f=bithumb_bch -v
