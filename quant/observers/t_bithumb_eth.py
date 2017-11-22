81acae633cbdf8e1d7868e8459f91cf200f6e0ca
doubleDragon <aiai373824745@gmail.com>
2017-11-22 18:56:51 +0800

    Fix param for observer t_bithumb_eth

--- quant/observers/t_bithumb_eth.py
+++ quant/observers/t_bithumb_eth.py
@@ -8,7 +8,7 @@ from .t_bithumb import T_Bithumb
 class T_Bithumb_ETH(T_Bithumb):
     """
     bch:
-    python -m quant.cli -mBithumb_ETH_KRW,Bitfinex_ETH_BTC,Bithumb_BTC_KRW -o=T_Bithumb_ETH -f=bithumb_eth -v
+    ./venv/bin/python -m quant.cli -mBithumb_ETH_KRW,Bitfinex_ETH_BTC,Bithumb_BTC_KRW -o=T_Bithumb_ETH -f=bithumb_eth -v
 
     目前的限制:
     """
@@ -24,8 +24,9 @@ class T_Bithumb_ETH(T_Bithumb):
             'fee_base': 0.0015,
             'fee_pair1': 0.002,
             'fee_pair2': 0.0015,
-            'min_amount_market': 0.04,
-            'min_amount_mid': 0.004,
+            'min_stock_base': 0.0,
+            'min_stock_pair1': 0.01,
+            'min_stock_pair2': 0.001,
             'max_trade_amount': 1,
             'min_trade_amount': 0.04
         }
