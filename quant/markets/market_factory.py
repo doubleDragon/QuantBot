edfbae39f1baac63971d7239e6f5ff181095ad30
doubleDragon <aiai373824745@gmail.com>
2017-11-28 19:15:23 +0800

    Fix market_factory about observer liquid

--- quant/markets/market_factory.py
+++ quant/markets/market_factory.py
@@ -68,7 +68,7 @@ def create_markets(exchange_names):
         elif name == "%s_IOT_BTC" % constant.EX_BFX:
             ex = Bitfinex('iotbtc')
         elif name == "%s_BCC_BTC" % constant.EX_KKEX:
-            ex = Kkex('BCCBTC')
+            ex = Kkex('BCHBTC')
         elif name == "%s_BCC_BTC" % constant.EX_LQ:
             ex = Liqui('bcc_btc')
         elif name == "%s_BCC_ETH" % constant.EX_LQ:
